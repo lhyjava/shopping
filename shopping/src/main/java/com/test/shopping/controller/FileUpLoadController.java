deace01f16e13800162d196e8306f226792797b2
EDZ <EDZ@WINDOWS-DTOI59N>
2019-11-12 10:32:19 +0800

    文件上传变更：新增log日志

--- shopping/src/main/java/com/test/shopping/controller/FileUpLoadController.java
+++ shopping/src/main/java/com/test/shopping/controller/FileUpLoadController.java
@@ -1,11 +1,15 @@
 package com.test.shopping.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.lang.ProcessBuilder.Redirect;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -34,6 +38,7 @@ public class FileUpLoadController {
 	}
 	
 	private final static String filePath = "E://data/";
+	private final static String logPath = "E://data/log.txt";
 	
 	@SuppressWarnings("unchecked")
 	@RequestMapping("/fileupload")
@@ -52,23 +57,43 @@ public class FileUpLoadController {
         String strDateFormat = "yyyy-MM-dd HH:mm:ss";
         SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
         
-        System.err.println("*******************************");
-        System.out.println("姓名：" + name);
-        System.out.println("文件名： " + fileName);
-        System.out.println("上传时间：" + sdf.format(date));
-        System.err.println("*******************************");
+        /**
+         * 生成log文件
+         */
+        List<Map<String,String>> list = new ArrayList<Map<String,String>>();
+        Map<String,String> logMap = new HashMap<String, String>();
+        logMap.put("姓名", name);
+        logMap.put("文件名", fileName);
+        logMap.put("上传时间", sdf.format(date));
+        list.add(logMap);
+        
+    	try {
+			File logfile = new File(logPath);
+			FileOutputStream fos = null;
+			if(!logfile.exists()){
+				logfile.createNewFile();
+				fos = new FileOutputStream(logfile);
+			}else{
+			    fos = new FileOutputStream(logfile,true);
+			}
+			OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
+			
+			for (Map<String, String> map : list) {
+				
+				for (Map.Entry<String, String> entry : logMap.entrySet()) {
+					String str = entry.getKey() + "：" + entry.getValue();
+                    osw.write(str + ",");
+				}
+				osw.write("\r\n");
+			}
+			osw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        /*
-        // 文件后缀
-        String suffixName = fileName.substring(fileName.lastIndexOf("."));
-        System.out.println("文件后缀名： " + suffixName);
-
-        // 重新生成唯一文件名，用于存储数据库
-        String newFileName = UUID.randomUUID().toString()+suffixName;
-        System.out.println("新的文件名： " + newFileName);
-		*/
-		
-        //创建文件
+        /**
+         * 文件上传
+         */
         File dest = new File(filePath + fileName);
 
         Map map = new HashMap();
